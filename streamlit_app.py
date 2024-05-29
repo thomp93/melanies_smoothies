ef26f39a12b90c763fc3d8901dfa068ee6c94c3c
thomp93 <159131227+thomp93@users.noreply.github.com>
2024-05-29 10:38:19 +0200

    Update streamlit_app.py
    moved into loop

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,8 @@ if ingredient_list:
     ingredients_string = ''
     for ingredient in ingredient_list:
         ingredients_string += ingredient + " "
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
