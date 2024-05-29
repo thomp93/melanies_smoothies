ed3fc36ddd3f6fdf8a45d1df8d721cbb48e20c96
thomp93 <159131227+thomp93@users.noreply.github.com>
2024-05-29 11:04:21 +0200

    Update streamlit_app.py
    added pd comparison

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,10 @@ if ingredient_list:
     ingredients_string = ''
     for ingredient in ingredient_list:
         ingredients_string += ingredient + " "
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(ingredient + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + ingredient)
         fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
